56cb3ad6fab3755e50a4c0b14d89e7a10b76337f
Brandon Wagner <bmwagner10@gmail.com>
2015-11-24 18:56:28 -0500

    added branches to master in separate directories

---
+++ OAuth2/src/main/java/edu/gmu/isa656/PersonRepository.java
@@ -0,0 +1,28 @@
+package edu.gmu.isa656;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.List;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    Page<Person> findAll(Pageable pageable);
+
+    @Override
+    @PostAuthorize("returnObject.firstName == principal.username or hasRole('ROLE_ADMIN')")
+    Person findOne(Long aLong);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    List<Person> findByFirstNameLike(@Param("firstName") String firstName);
+
+    @RestResource(exported = false)
+    Person findByFirstNameEquals(String firstName);
+}
